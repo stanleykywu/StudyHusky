9118597e8e97e89bd6aca454a27a5d5644c45db4
stanleykywu <stanleykywu@gmail.com>
2020-02-07 23:34:26 -0500

    revert

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn you rmoms
+          Learn more!
         </a>
       </header>
     </div>
