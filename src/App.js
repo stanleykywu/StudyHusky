7124ea1da45a0ab276b9490b38f292a72dadb604
stanleykywu <stanleykywu@gmail.com>
2020-02-08 10:30:57 -0500

    added second state field

--- src/App.js
+++ src/App.js
@@ -7,16 +7,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      floor: null
+      floor: null,
+      occupied: null
     };
   }
 
   //this is called after inital DOM
   componentWillMount() {
-    const floorRef = firebase.database().ref().child('Classroom').child('SN11').child('floor');
+    const roomRef = firebase.database().ref().child('Classroom').child('SN12');
+    const floorRef = roomRef.child('floor');
+    const occRef = roomRef.child('occupied')
     floorRef.on('value', snap => {
       this.setState({
         floor: snap.val(),
+      });
+    });
+    occRef.on('value', snap => {
+      this.setState({
+        occupied: snap.val(),
       });
     });
   }
@@ -24,7 +32,7 @@ class App extends Component {
   handleSubmit(e) {
     e.preventDefault();
 
-    firebase.database().ref().child('Classroom').child('SN11').update({
+    firebase.database().ref().child('Classroom').child('SN12').update({
       occupied: 'vv',
       section: 'A14',
       floor: 1
@@ -39,13 +47,12 @@ class App extends Component {
           <script src="https://cdn.firebase.com/js/client/2.4.2/firebase.js"></script>
         </head>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <button onClick={this.handleSubmit} >Occupied/Leave</button>
           <p>
             {this.state.floor}
+          </p>
+          <p>
+            {this.state.occupied}
           </p>
         </header>
       </div>
