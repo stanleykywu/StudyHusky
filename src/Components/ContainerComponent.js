8f13188a88ba8d13f98aaa7fc85f515f3fa0ad60
stanleykywu <stanleykywu@gmail.com>
2020-02-08 17:31:12 -0500

    corrected indent

--- src/Components/ContainerComponent.js
+++ src/Components/ContainerComponent.js
@@ -5,104 +5,104 @@ import {ButtonDropdown} from "reactstrap";
 
 
 class ContainerComponent extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            id: '',
-            occupied: false,
-            room: 'Select room'
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleRoomChange = this.handleRoomChange.bind(this);
+    this.state = {
+      id: '',
+      occupied: false,
+      room: 'Select room'
     }
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRoomChange = this.handleRoomChange.bind(this);
+  }
 
 
-    handleChange(e) {
-        this.setState({ [e.target.name]: e.target.value }, () => {
-            console.log(this.state.id);
-        });
-    }
 
-    handleSubmit(e) {
-        this.setState({ occupied: true}, () => {
-            alert(this.state.room + ' ' + this.state.id);
-        });
-        //this.props.onLocChange(this.state.room);
-        this.props.populate(e);
-        e.preventDefault();
-    }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      console.log(this.state.id);
+    });
+  }
 
-    handleRoomChange(roomVal) {
-        this.setState({ room: roomVal }, () => {
-            this.props.onLocChange(this.state.room);
-        });
-    }
+  handleSubmit(e) {
+    this.setState({ occupied: true}, () => {
+      alert(this.state.room + ' ' + this.state.id);
+    });
+    //this.props.onLocChange(this.state.room);
+    this.props.populate(e);
+    e.preventDefault();
+  }
 
-    render() {
-        return (
-            <div className='m-5'>
-                <Form onSubmit={this.handleSubmit}>
-                    <FormGroup>
-                        <Label>Please enter your NUID</Label>
-                        <Input type="text" name="id" placeholder="000000000" onChange={(this.handleChange)}/>
-                    </FormGroup>
-                    {!this.state.occupied && <DropdownComponent2 onRoomChange={this.handleRoomChange} roomNumber={this.state.room}/>}
-                    {this.state.occupied && <Alert color="success">Occupied!</Alert>}
-                    {!this.state.occupied && <Button type="submit" color="success">Occupy this room</Button>}
-                </Form>
-            </div>
-        );
-    }
+  handleRoomChange(roomVal) {
+    this.setState({ room: roomVal }, () => {
+      this.props.onLocChange(this.state.room);
+    });
+  }
+
+  render() {
+    return (
+      <div className='m-5'>
+        <Form onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <Label>Please enter your NUID</Label>
+            <Input type="text" name="id" placeholder="000000000" onChange={(this.handleChange)}/>
+          </FormGroup>
+          {!this.state.occupied && <DropdownComponent2 onRoomChange={this.handleRoomChange} roomNumber={this.state.room}/>}
+          {this.state.occupied && <Alert color="success">Occupied!</Alert>}
+          {!this.state.occupied && <Button type="submit" color="success">Occupy this room</Button>}
+        </Form>
+      </div>
+    );
+  }
 }
 
 class DropdownComponent2 extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.toggle = this.toggle.bind(this);
-        this.changeValue = this.changeValue.bind(this);
-        this.state = {
-            dropDownValue: 'Select location',
-            dropdownOpen: false
-        };
-    }
+    this.toggle = this.toggle.bind(this);
+    this.changeValue = this.changeValue.bind(this);
+    this.state = {
+      dropDownValue: 'Select location',
+      dropdownOpen: false
+    };
+  }
 
-    toggle(event) {
+  toggle(event) {
 
-        this.setState({
-            dropdownOpen: !this.state.dropdownOpen
-        });
-    }
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
 
-    changeValue(e) {
-        this.props.onRoomChange(e.currentTarget.textContent);
-        // this.setState({dropDownValue: e.currentTarget.textContent}, () => {
-        //     console.log(this.state.dropDownValue);
-        // });
-        // this.setState({dropDownValue: e.currentTarget.textContext}, () => {
-        //         this.props.onRoomChange(e.currentTarget.textContent);
-        //     })
-    }
+  changeValue(e) {
+    this.props.onRoomChange(e.currentTarget.textContent);
+    // this.setState({dropDownValue: e.currentTarget.textContent}, () => {
+    //     console.log(this.state.dropDownValue);
+    // });
+    // this.setState({dropDownValue: e.currentTarget.textContext}, () => {
+    //         this.props.onRoomChange(e.currentTarget.textContent);
+    //     })
+  }
 
-    render() {
-        return (
-            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                <DropdownToggle caret color="primary">
-                    {this.props.roomNumber}
-                </DropdownToggle>
-                <DropdownMenu>
-                    <DropdownItem onClick={this.changeValue}>SN11</DropdownItem>
-                    <DropdownItem onClick={this.changeValue}>SN12</DropdownItem>
-                    <DropdownItem onClick={this.changeValue}>SN13</DropdownItem>
-                </DropdownMenu>
-            </ButtonDropdown>
-        );
-    }
+  render() {
+    return (
+      <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret color="primary">
+          {this.props.roomNumber}
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem onClick={this.changeValue}>SN11</DropdownItem>
+          <DropdownItem onClick={this.changeValue}>SN12</DropdownItem>
+          <DropdownItem onClick={this.changeValue}>SN13</DropdownItem>
+        </DropdownMenu>
+      </ButtonDropdown>
+    );
+  }
 
 }
 
